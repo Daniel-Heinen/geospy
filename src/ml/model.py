a2630d8effc4fffa6e4e19c83163c5fa69c7d4c6
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-01-17 17:54:43 +0000

    refactor: improve error handling

---
+++ src/ml/model.py
@@ -0,0 +1,14 @@
+"""Machine learning model for geolocation prediction"""
+import torch
+import torch.nn as nn
+
+class GeoSpyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 64, 3)
+        self.fc = nn.Linear(64, 2)
+    
+    def forward(self, x):
+        return self.fc(self.conv1(x))
+# Modified 2025-07-03
+# Modified 2025-10-28
