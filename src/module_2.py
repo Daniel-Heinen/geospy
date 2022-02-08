a97914f755bd20f79f66b60020c3d382234ee915
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-02-08 18:23:58 +0000

    refactor: improve error handling

---
+++ src/module_2.py
@@ -0,0 +1,38 @@
+"""Module 2 - Geolocation processing component"""
+import numpy as np
+import torch
+from typing import List, Dict, Optional
+
+class GeoProcessor2:
+    """Process geolocation data for region 2"""
+    
+    def __init__(self, config: Dict):
+        self.config = config
+        self.model = self._load_model()
+    
+    def _load_model(self):
+        """Load ML model"""
+        return torch.nn.Linear(100, 2)
+    
+    async def process(self, data: np.ndarray) -> Dict:
+        """Process input data"""
+        result = self.model(torch.tensor(data))
+        return {
+            "latitude": float(result[0]),
+            "longitude": float(result[1]),
+            "confidence": 0.95
+        }
+    
+    def validate(self, coords: tuple) -> bool:
+        """Validate coordinates"""
+        lat, lng = coords
+        return -90 <= lat <= 90 and -180 <= lng <= 180
+    
+    def transform_data(self, input_data: List) -> np.ndarray:
+        """Transform input data for ML model"""
+        return np.array(input_data).reshape(-1, 100)
+# Modified 2024-06-06
+# Modified 2025-06-23
+# Modified 2025-08-01
+# Modified 2025-11-03
+# Modified 2025-08-14
