c94dbd5844c8fe2d36f61a9c0d446f84a392e184
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-04-14 18:56:06 +0000

    feat: add core functionality

---
+++ tests/test_module_1.py
@@ -0,0 +1,26 @@
+"""Tests for module 1"""
+import pytest
+import numpy as np
+from src.module_1 import GeoProcessor1
+
+def test_processor_init():
+    processor = GeoProcessor1({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor1({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor1({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-06-09
+# Modified 2025-07-16
+# Modified 2025-07-08
+# Modified 2025-08-18
+# Modified 2025-08-03
