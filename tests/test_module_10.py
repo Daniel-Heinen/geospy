1eef511157872d286b5bc80294109667e13d8369
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-04-15 19:29:44 +0000

    feat: improve ML model

---
+++ tests/test_module_10.py
@@ -0,0 +1,23 @@
+"""Tests for module 10"""
+import pytest
+import numpy as np
+from src.module_10 import GeoProcessor10
+
+def test_processor_init():
+    processor = GeoProcessor10({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor10({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor10({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-08-29
+# Modified 2025-07-16
