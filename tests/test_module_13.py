accb553e95010f831f10e3d86de8613a006cbbf7
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-04-22 10:22:48 +0000

    chore: configure CI/CD

---
+++ tests/test_module_13.py
@@ -0,0 +1,26 @@
+"""Tests for module 13"""
+import pytest
+import numpy as np
+from src.module_13 import GeoProcessor13
+
+def test_processor_init():
+    processor = GeoProcessor13({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor13({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor13({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-06-25
+# Modified 2025-07-19
+# Modified 2025-08-04
+# Modified 2025-10-08
+# Modified 2025-08-11
