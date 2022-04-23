e4d08566a39217b131d717a6f62f7d2bce94e0dd
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-04-23 18:09:43 +0000

    feat: add API endpoints

---
+++ tests/test_module_14.py
@@ -0,0 +1,26 @@
+"""Tests for module 14"""
+import pytest
+import numpy as np
+from src.module_14 import GeoProcessor14
+
+def test_processor_init():
+    processor = GeoProcessor14({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor14({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor14({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2024-07-04
+# Modified 2025-07-23
+# Modified 2025-10-03
+# Modified 2025-06-17
+# Modified 2025-08-05
