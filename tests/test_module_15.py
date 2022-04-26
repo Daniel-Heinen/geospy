25d73382994ef57392ff75c6259f05adfd04b51e
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-04-26 10:27:38 +0000

    perf: optimize queries

---
+++ tests/test_module_15.py
@@ -0,0 +1,25 @@
+"""Tests for module 15"""
+import pytest
+import numpy as np
+from src.module_15 import GeoProcessor15
+
+def test_processor_init():
+    processor = GeoProcessor15({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor15({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor15({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-06-24
+# Modified 2025-09-11
+# Modified 2025-08-12
+# Modified 2025-06-16
