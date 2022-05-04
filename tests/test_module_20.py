78db4d893ddecb7bf8d1d6d67e08a3a82b9aca42
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-04 10:52:29 +0000

    perf: improve response time

---
+++ tests/test_module_20.py
@@ -0,0 +1,21 @@
+"""Tests for module 20"""
+import pytest
+import numpy as np
+from src.module_20 import GeoProcessor20
+
+def test_processor_init():
+    processor = GeoProcessor20({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor20({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor20({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
