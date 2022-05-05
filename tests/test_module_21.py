af588512e3ccea2f76d6beee7b4dd07d4754b938
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-05 13:27:14 +0000

    docs: add examples

---
+++ tests/test_module_21.py
@@ -0,0 +1,28 @@
+"""Tests for module 21"""
+import pytest
+import numpy as np
+from src.module_21 import GeoProcessor21
+
+def test_processor_init():
+    processor = GeoProcessor21({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor21({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor21({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-08-25
+# Modified 2025-09-26
+# Modified 2025-10-24
+# Modified 2025-07-02
+# Modified 2025-08-21
+# Modified 2025-10-22
+# Modified 2025-06-17
