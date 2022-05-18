7e1d3bf9c0693f37ef1d69d89f1f54cf6d3aac60
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-18 12:53:57 +0000

    refactor: optimize performance

---
+++ tests/test_module_3.py
@@ -0,0 +1,25 @@
+"""Tests for module 3"""
+import pytest
+import numpy as np
+from src.module_3 import GeoProcessor3
+
+def test_processor_init():
+    processor = GeoProcessor3({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor3({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor3({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2024-06-21
+# Modified 2025-08-15
+# Modified 2025-06-21
+# Modified 2025-10-01
