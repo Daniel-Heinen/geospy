fb7a9bdaacaa0a4610c7aec2f64bae77158f61cb
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-19 16:50:07 +0000

    test: add unit tests

---
+++ tests/test_module_30.py
@@ -0,0 +1,23 @@
+"""Tests for module 30"""
+import pytest
+import numpy as np
+from src.module_30 import GeoProcessor30
+
+def test_processor_init():
+    processor = GeoProcessor30({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor30({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor30({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-08-17
+# Modified 2025-08-12
