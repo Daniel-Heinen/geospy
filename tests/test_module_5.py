eab13046d04cfb271b18ca0f2fab246ab1d4846b
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-24 12:11:38 +0000

    fix: resolve authentication issue

---
+++ tests/test_module_5.py
@@ -0,0 +1,24 @@
+"""Tests for module 5"""
+import pytest
+import numpy as np
+from src.module_5 import GeoProcessor5
+
+def test_processor_init():
+    processor = GeoProcessor5({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor5({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor5({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-08-02
+# Modified 2025-07-01
+# Modified 2025-08-08
