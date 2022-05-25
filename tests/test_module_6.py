75ac7a0bcf4195ed7c611c9f43e75b4695079ad9
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-25 14:26:47 +0000

    feat: implement data processing

---
+++ tests/test_module_6.py
@@ -0,0 +1,24 @@
+"""Tests for module 6"""
+import pytest
+import numpy as np
+from src.module_6 import GeoProcessor6
+
+def test_processor_init():
+    processor = GeoProcessor6({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor6({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor6({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-07-30
+# Modified 2025-08-12
+# Modified 2025-11-04
