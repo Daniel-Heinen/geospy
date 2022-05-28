2748e17da5592f2b5ada721108433d5766603941
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-28 15:24:38 +0000

    refactor: improve error handling

---
+++ tests/test_module_7.py
@@ -0,0 +1,23 @@
+"""Tests for module 7"""
+import pytest
+import numpy as np
+from src.module_7 import GeoProcessor7
+
+def test_processor_init():
+    processor = GeoProcessor7({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor7({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor7({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-08-09
+# Modified 2025-09-25
