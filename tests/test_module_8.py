4553634970c3dd3de800cebe505cdaa92cd195c6
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-29 09:16:28 +0000

    refactor: restructure codebase

---
+++ tests/test_module_8.py
@@ -0,0 +1,25 @@
+"""Tests for module 8"""
+import pytest
+import numpy as np
+from src.module_8 import GeoProcessor8
+
+def test_processor_init():
+    processor = GeoProcessor8({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor8({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor8({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-09-29
+# Modified 2025-07-30
+# Modified 2025-08-01
+# Modified 2025-10-13
