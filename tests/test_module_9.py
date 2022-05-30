24e0233ca540292c9f3a280fcfa4b44688194016
Daniel-Heinen <254339149+Daniel-Heinen@users.noreply.github.com>
2022-05-30 13:46:40 +0000

    security: add validation

---
+++ tests/test_module_9.py
@@ -0,0 +1,24 @@
+"""Tests for module 9"""
+import pytest
+import numpy as np
+from src.module_9 import GeoProcessor9
+
+def test_processor_init():
+    processor = GeoProcessor9({})
+    assert processor is not None
+
+def test_validate_coords():
+    processor = GeoProcessor9({})
+    assert processor.validate((0, 0)) == True
+    assert processor.validate((91, 0)) == False
+
+@pytest.mark.asyncio
+async def test_process():
+    processor = GeoProcessor9({})
+    data = np.random.rand(100)
+    result = await processor.process(data)
+    assert "latitude" in result
+    assert "longitude" in result
+# Modified 2025-07-22
+# Modified 2025-07-28
+# Modified 2025-10-27
